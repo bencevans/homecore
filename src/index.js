0f9ecb0373f1bd753965cd088ca36c46f5de3e0a
Ben Evans <ben@bluechimp.io>
2019-01-02 14:22:56 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const homedir = require('os').homedir();
+const archiver = require('hypercore-archiver')
+const swarm = require('hypercore-archiver/swarm')
+
+const ar = archiver(path.resolve(homedir, '.homecore'))
+const sw = swarm(ar);
+
+(process.env.CORES || '')
+    .split(',')
+    .filter((feedKey) => {
+        return typeof feedKey === 'string' && feedKey.length === 64
+    })
+    .forEach(feedKey => {
+    console.log('feedKey: ', feedKey)
+    ar.add(Buffer.from(feedKey, 'hex'))
+})
+
+ar.on('sync', function (feed) {
+    console.log('feed is synced', feed.key)
+})
